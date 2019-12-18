fcd31891ea3aec481534e0fadf7c10c34988f85d
Sławomir Chołaj <slawomir.cholaj@gmail.com>
2019-12-18 20:43:08 +0100

    Delete install_in_docker.sh

--- install_in_docker.sh
+++
@@ -1,9 +0,0 @@
-#mkdir /root/.pytigon
-mkdir /root/.pytigon/ext_prg
-curl -L -o /root/.pytigon/ext_prg/tcc-0.9.26.tar.bz2 http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.26.tar.bz2
-tar xjf /root/.pytigon/ext_prg/tcc-0.9.26.tar.bz2 -C /root/.pytigon/ext_prg/
-rm /root/.pytigon/ext_prg/tcc-0.9.26.tar.bz2
-mv /root/.pytigon/ext_prg/tcc-0.9.26 /root/.pytigon/ext_prg/tcc
-cd /root/.pytigon/ext_prg/tcc
-./configure --disable-static
-make
